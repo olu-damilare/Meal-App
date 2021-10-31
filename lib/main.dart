92e132e02262437bd09bbc535652a1681739f7e8
Damilare Jolayemi <djolayemi@gmail.com>
2021-10-31 01:30:27 +0100

    added more routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/screens/category_meals_screen.dart';
+import 'package:meal_app/screens/filters_screen.dart';
+import 'package:meal_app/screens/meal_details.dart';
+import 'package:meal_app/screens/tabs_screen.dart';
+
+import 'screens/categories_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,32 +19,37 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
+        accentColor: Colors.orange,
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          bodyText1: const TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1)
+          ),
+          bodyText2: const TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1)
+          ),
+          subtitle1: const TextStyle(
+            fontSize: 20,
+            fontFamily: 'RobotoCondensed',
+            fontWeight: FontWeight.bold
+          )
+        )
       ),
-      home: const MyHomePage(title: 'DeliMeals'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-       return Scaffold(
-      appBar: AppBar(
-               title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Text('Navigation Time!'),
-      ),
+      // home: CategoriesScreen(),
+      routes: {
+        '/': (ctx) => TabsScreen(),
+        FiltersScreen.routeName : (ctx) => FiltersScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        MealDetailsScreen.routeName: (ctx) => MealDetailsScreen()
+      },
+      onGenerateRoute: (settings){
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      },
+      onUnknownRoute: (settings){
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      },
     );
   }
 }
