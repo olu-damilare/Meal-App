ece0650ccd2bed914959828c29c10a5f8d8192c5
Damilare Jolayemi <djolayemi@gmail.com>
2021-10-31 01:31:20 +0100

    added drawer

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/widgets/main_drawer.dart';
 
 class FiltersScreen extends StatelessWidget {
   const FiltersScreen({Key? key}) : super(key: key);
@@ -8,6 +9,7 @@ class FiltersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: MainDrawer(),
         body: Center(child: Text('Filters'),));
   }
 }
