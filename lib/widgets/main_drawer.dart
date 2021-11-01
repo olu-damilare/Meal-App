454a1144bfe453b06d60e6260af4d8d916a0b38e
Damilare Jolayemi <djolayemi@gmail.com>
2021-11-01 22:28:48 +0100

    meal details

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -42,10 +42,10 @@ class MainDrawer extends StatelessWidget {
           ),
           const SizedBox(height: 20),
           buildListTile('Meal', Icons.restaurant, () {
-            Navigator.of(context).pushNamed('/');
+            Navigator.of(context).pushReplacementNamed('/');
           }),
           buildListTile('Filters', Icons.settings, () {
-            Navigator.of(context).pushNamed(FiltersScreen.routeName);
+            Navigator.of(context).pushReplacementNamed(FiltersScreen.routeName);
 
           }),
 
