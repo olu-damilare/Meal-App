f27a56021527798bff2c9ea1a8ff1d42e5d6f8ea
Damilare Jolayemi <djolayemi@gmail.com>
2021-11-09 03:09:50 +0100

    update

--- lib/screens/favourites_screen.dart
+++ lib/screens/favourites_screen.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/models/meal.dart';
+import 'package:meal_app/widgets/meal_item.dart';
 
 class FavouritesScreen extends StatelessWidget {
-  const FavouritesScreen({Key? key}) : super(key: key);
+
+  final List<Meal> favouriteMeals;
+
+  FavouritesScreen(this.favouriteMeals);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("The favourites")
-    );
+    if (favouriteMeals.isEmpty) {
+      return const Center(
+          child: Text("You have no favorites yet - start adding some")
+      );
+    }else{
+      return ListView.builder(
+        itemBuilder: (ctx, index){
+          return MealItem(
+            id: favouriteMeals[index].id,
+            title: favouriteMeals[index].title,
+            imageUrl: favouriteMeals[index].imageUrl,
+            duration: favouriteMeals[index].duration,
+            affordability: favouriteMeals[index].affordability,
+            complexity: favouriteMeals[index].complexity,
+          );
+        },
+        itemCount: favouriteMeals.length
+        ,);
+    }
+
   }
 }
